7ad742982704c01115f05013227754a7b275cdd9
Яков Хватов <YakovX95@mail.ru>
2024-12-02 23:23:50 +0300

    Added DI for db and minimal auth with TODO: jwt

--- routes/routes.go
+++ routes/routes.go
@@ -3,9 +3,20 @@ package routes
 import (
 	"BananaStream.API/controllers"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
-func SetupRoutes(app *fiber.App) {
+func SetupRoutes(app *fiber.App, db *gorm.DB) {
+
+	//livekit
 	app.Post("/getToken", controllers.Token)
 	app.Get("/rooms", controllers.Rooms)
+
+	//auth
+	app.Post("/login", func(c *fiber.Ctx) error {
+		return controllers.Login(c, db)
+	})
+	app.Post("/register", func(c *fiber.Ctx) error {
+		return controllers.Register(c, db)
+	})
 }
